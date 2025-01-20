32c7828200ced449ac66c65f7b8f285e9a242c9a
Chyharov <chyharov@gmail.com>
2025-01-20 22:05:26 +0100

    START: upload base setup and markup

---
+++ src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,17 @@
+import header from 'components/header/header';
+import componentForMainPage from 'components/componentForMainPage/componentForMainPage';
+
+const MainPage = () => {
+  return (
+    <>
+      <header />
+        <main>
+        <componentForMainPage />      
+      </main>
+    </>
+  );
+};
+
+
+
+export default MainPage;
---
+++ src/pages/SecondPage/SecondPage.jsx
@@ -0,0 +1,19 @@
+import header from 'components/header/header';
+import secondPageComponent from 'components/secondPageComponent/secondPageComponent';
+
+
+
+const SecondPage = () => {
+  return (
+    <>
+        <header />
+    <main>
+        <secondPageComponent />
+    </main>
+    </>
+  );
+};
+
+
+
+export default SecondPage;
