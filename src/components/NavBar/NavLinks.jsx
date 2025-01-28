530ed9166eaa51c16d3ccb82c9602cd6715dfe73
Chyharov <chyharov@gmail.com>
2025-01-28 22:34:22 +0100

    ADDED: logic for open and close search

--- src/components/NavBar/NavLinks.jsx
+++ src/components/NavBar/NavLinks.jsx
@@ -29,105 +29,107 @@ const NavLinks = props => {
   };
 
   return (
-    <div className={'container ' + s.menuList__container}>
-      <ul className={s.menu__list}>
-        <Link
-          onClick={() => props.isMobile && props.closeMobileMenu()}
-          className={s.menu__list_link}
-          to="/"
-          spy={true}
-          smooth={true}
-          offset={getOffsetByDeviceType()}
-          duration={500}
-        >
-          <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{ delay: 0.05 }}
-            className={s.menu__list_item}
-          >
-            Про нас
-          </motion.li>
-        </Link>
-
-        <Link
-          onClick={() => props.isMobile && props.closeMobileMenu()}
-          className={s.menu__list_link}
-          to="/about"
-          spy={true}
-          smooth={true}
-          offset={getOffsetByDeviceType()}
-          duration={500}
-        >
-          <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{ delay: 0.1 }}
-            className={s.menu__list_item}
-          >
-            Товари
-          </motion.li>
-        </Link>
-
-        <Link
-          onClick={() => props.isMobile && props.closeMobileMenu()}
-          className={s.menu__list_link}
-          to="/ritualgoods"
-          spy={true}
-          smooth={true}
-          offset={getOffsetByDeviceType()}
-          duration={500}
-        >
-          <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{ delay: 0.2 }}
-            className={s.menu__list_item}
+    <div className={s.bgShadow}>
+      <div className={'container ' + s.menuList__container}>
+        <ul className={s.menu__list}>
+          <Link
             onClick={() => props.isMobile && props.closeMobileMenu()}
+            className={s.menu__list_link}
+            to="/"
+            spy={true}
+            smooth={true}
+            offset={getOffsetByDeviceType()}
+            duration={500}
           >
-            Торгові марки
-          </motion.li>
-        </Link>
+            <motion.li
+              initial={animateFrom}
+              animate={animateTo}
+              transition={{ delay: 0.05 }}
+              className={s.menu__list_item}
+            >
+              Про нас
+            </motion.li>
+          </Link>
 
-        <Link
-          onClick={() => props.isMobile && props.closeMobileMenu()}
-          className={s.menu__list_link}
-          to="/ritualtransport"
-          spy={true}
-          smooth={true}
-          offset={getOffsetByDeviceType()}
-          duration={500}
-        >
-          <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{ delay: 0.3 }}
-            className={s.menu__list_item}
+          <Link
             onClick={() => props.isMobile && props.closeMobileMenu()}
+            className={s.menu__list_link}
+            to="/about"
+            spy={true}
+            smooth={true}
+            offset={getOffsetByDeviceType()}
+            duration={500}
           >
-            Команда
-          </motion.li>
-        </Link>
+            <motion.li
+              initial={animateFrom}
+              animate={animateTo}
+              transition={{ delay: 0.1 }}
+              className={s.menu__list_item}
+            >
+              Товари
+            </motion.li>
+          </Link>
 
-        <Link
-          onClick={() => props.isMobile && props.closeMobileMenu()}
-          className={s.menu__list_link}
-          to="/ritualservices"
-          spy={true}
-          smooth={true}
-          offset={getOffsetByDeviceType()}
-          duration={500}
-        >
-          <motion.li
-            initial={animateFrom}
-            animate={animateTo}
-            transition={{ delay: 0.4 }}
-            className={s.menu__list_item}
+          <Link
+            onClick={() => props.isMobile && props.closeMobileMenu()}
+            className={s.menu__list_link}
+            to="/ritualgoods"
+            spy={true}
+            smooth={true}
+            offset={getOffsetByDeviceType()}
+            duration={500}
           >
-            Інновації
-          </motion.li>
-        </Link>
-      </ul>
+            <motion.li
+              initial={animateFrom}
+              animate={animateTo}
+              transition={{ delay: 0.2 }}
+              className={s.menu__list_item}
+              onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
+              Торгові марки
+            </motion.li>
+          </Link>
+
+          <Link
+            onClick={() => props.isMobile && props.closeMobileMenu()}
+            className={s.menu__list_link}
+            to="/ritualtransport"
+            spy={true}
+            smooth={true}
+            offset={getOffsetByDeviceType()}
+            duration={500}
+          >
+            <motion.li
+              initial={animateFrom}
+              animate={animateTo}
+              transition={{ delay: 0.3 }}
+              className={s.menu__list_item}
+              onClick={() => props.isMobile && props.closeMobileMenu()}
+            >
+              Команда
+            </motion.li>
+          </Link>
+
+          <Link
+            onClick={() => props.isMobile && props.closeMobileMenu()}
+            className={s.menu__list_link}
+            to="/ritualservices"
+            spy={true}
+            smooth={true}
+            offset={getOffsetByDeviceType()}
+            duration={500}
+          >
+            <motion.li
+              initial={animateFrom}
+              animate={animateTo}
+              transition={{ delay: 0.4 }}
+              className={s.menu__list_item}
+            >
+              Інновації
+            </motion.li>
+          </Link>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/NavSearch/NavSearch.jsx
+++ src/components/NavSearch/NavSearch.jsx
@@ -1,12 +1,46 @@
+import React, { useState } from 'react';
 import searchIcon from 'images/header/searchIcon.svg';
 import s from './NavSearch.module.scss';
 
 const NavSearch = () => {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+  const toggleSearch = () => {
+    setIsSearchOpen(prev => !prev);
+  };
+
+  const closeSearch = () => {
+    setIsSearchOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
+  const handleOutsideClick = event => {
+    if (event.target === event.currentTarget) {
+      closeSearch();
+    }
+  };
+
   return (
-      <>
-        <img className={s.searchIcon} src={searchIcon} alt="searchIcon" />
-      </>
-  )
+    <>
+      <button
+        className={s.navSearch__button}
+        aria-label="Search"
+        onClick={toggleSearch}
+      >
+        <img className={s.searchIcon} src={searchIcon} alt="Search" />
+      </button>
+
+      {isSearchOpen && (
+        <div className={s.bgShadow} onClick={handleOutsideClick}>
+          <input
+            type="text"
+            className={s.searchInput}
+            placeholder="Type to search..."
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default NavSearch;
