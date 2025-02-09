4694d9669388703cfe2f289f638b1cf095752263
Chyharov <chyharov@gmail.com>
2025-02-09 16:00:57 +0100

    ADDED: markup and styles SectionHero

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,10 +1,22 @@
+import horseHeroImage from 'images/hero/horseHeroImage.jpg';
+import ankonaText from 'images/hero/ankonaText.svg';
 import s from './SectionHero.module.scss';
 
 const SectionHero = () => {
   return (
     <section className={s.sectionHero}>
+      <div className={s.heroPhotoContainer}>
+        <img
+          className={s.horseHeroImage}
+          src={horseHeroImage}
+          alt="horseHeroImage"
+        />
+        <img className={s.ankonaText} src={ankonaText} alt="ankonaText" />
+      </div>
       <div className={`container ${s.sectionHero__container}`}>
-        <h1>SectionHero</h1>
+        <div className={s.heroBorder}><div className={s.heroBorderDot}></div></div>
+        <p className={s.heroDescription}>Нова стратегія раціональної годівлі</p>
+        <button className={s.heroButton}>Зв’язатися</button>
       </div>
     </section>
   );
