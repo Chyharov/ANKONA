2f535fdaa64573b425d98aafd5dd04553a8956f0
Chyharov <chyharov@gmail.com>
2025-02-19 22:52:22 +0100

    ADDED: poducts array for SectionGoods

--- src/components/SectionGoods/SectionGoods.jsx
+++ src/components/SectionGoods/SectionGoods.jsx
@@ -4,177 +4,8 @@ import arrowDown from 'images/goods/arrowDown.svg';
 import arrowLeft from 'images/goods/arrowLeft.svg';
 import arrowRight from 'images/goods/arrowRight.svg';
 import noImages from 'images/goods/noMedia.jpg';
+import { products } from 'services/Events';
 import s from './SectionGoods.module.scss';
-
-const mockProducts = [
-  {
-    id: 1,
-    name: 'Mineralfluter',
-    category: 'Коні',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  { id: 2, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 3,
-    name: 'SuperFeed',
-    category: 'ВРХ дорослі',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 4, name: 'SuperFeed', category: 'Свині', manufacturer: 'ETOS' },
-  { id: 5, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 6,
-    name: 'Mineralfluter',
-    category: 'ВРХ молодняк',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  {
-    id: 7,
-    name: 'Mineralfluter',
-    category: 'Підходе для всіх',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  { id: 8, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  { id: 9, name: 'SuperFeed', category: 'ВРХ дорослі', manufacturer: 'ETOS' },
-  {
-    id: 10,
-    name: 'SuperFeed',
-    category: 'Свині',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 11, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 12,
-    name: 'Mineralfluter',
-    category: 'Підходе для всіх',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  {
-    id: 13,
-    name: 'Mineralfluter',
-    category: 'Коні',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  { id: 14, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 15,
-    name: 'SuperFeed',
-    category: 'Підходе для всіх',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 16, name: 'SuperFeed', category: 'Свині', manufacturer: 'ETOS' },
-  { id: 17, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 18,
-    name: 'Mineralfluter',
-    category: 'ВРХ молодняк',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  {
-    id: 19,
-    name: 'Mineralfluter',
-    category: 'Підходе для всіх',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  { id: 20, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  { id: 21, name: 'SuperFeed', category: 'ВРХ дорослі', manufacturer: 'ETOS' },
-  {
-    id: 22,
-    name: 'SuperFeed',
-    category: 'Підходе для всіх',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  {
-    id: 23,
-    name: 'Mineralfluter',
-    category: 'Коні',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  {
-    id: 24,
-    name: 'Mineralfluter',
-    category: 'Коні',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  { id: 25, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 26,
-    name: 'SuperFeed',
-    category: 'ВРХ дорослі',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 27, name: 'SuperFeed', category: 'Свині', manufacturer: 'ETOS' },
-  { id: 28, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 29,
-    name: 'Mineralfluter',
-    category: 'ВРХ молодняк',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  {
-    id: 30,
-    name: 'Mineralfluter',
-    category: 'Підходе для всіх',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  { id: 31, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  { id: 32, name: 'SuperFeed', category: 'ВРХ дорослі', manufacturer: 'ETOS' },
-  {
-    id: 33,
-    name: 'SuperFeed',
-    category: 'Свині',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 34, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 35,
-    name: 'Mineralfluter',
-    category: 'Підходе для всіх',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  {
-    id: 36,
-    name: 'Mineralfluter',
-    category: 'Коні',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  { id: 37, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 38,
-    name: 'SuperFeed',
-    category: 'Підходе для всіх',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 39, name: 'SuperFeed', category: 'Свині', manufacturer: 'ETOS' },
-  { id: 40, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 41,
-    name: 'Mineralfluter',
-    category: 'ВРХ молодняк',
-    manufacturer: 'AGRO-BIZEK',
-  },
-  {
-    id: 42,
-    name: 'Mineralfluter',
-    category: 'Підходе для всіх',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-  { id: 43, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  { id: 44, name: 'SuperFeed', category: 'ВРХ дорослі', manufacturer: 'ETOS' },
-  {
-    id: 45,
-    name: 'SuperFeed',
-    category: 'Підходе для всіх',
-    manufacturer: 'FUTTERGUT — надійна годівля',
-  },
-  { id: 46, name: 'Feed Mix', category: 'ДРХ', manufacturer: 'JRS' },
-  {
-    id: 47,
-    name: 'Mineralfluter',
-    category: 'Коні',
-    manufacturer: 'Anhoff FUTTERGUT',
-  },
-];
 
 const initialCategories = [
   'ВРХ дорослі',
@@ -185,6 +16,7 @@ const initialCategories = [
   'Свині',
   'Підходе для всіх',
 ];
+
 const initialManufacturers = [
   'AGRO-BIZEK',
   'Anhoff FUTTERGUT',
@@ -202,14 +34,14 @@ const SectionGoods = () => {
     manufacturer: new Set(),
   });
   
-  const [filteredProducts, setFilteredProducts] = useState(mockProducts);
+  const [filteredProducts, setFilteredProducts] = useState(products);
   const [currentPage, setCurrentPage] = useState(1);
   const [showCategories, setShowCategories] = useState(false);
   const [showManufacturers, setShowManufacturers] = useState(false);
   const itemsPerPage = 5;
 
   useEffect(() => {
-    setFilteredProducts(filterProducts(mockProducts, filters));
+    setFilteredProducts(filterProducts(products, filters));
     setCurrentPage(1);
   }, [filters]);
 
