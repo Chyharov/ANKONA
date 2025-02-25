b5a2bf2b70c17c2d4d013a042baa127db5096422
Chyharov <chyharov@gmail.com>
2025-02-25 21:29:54 +0100

    ADDED: Header, Markup and Styles for ProductDetails

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,9 +1,11 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { products } from 'services/Products';
+import Header from 'components/Header/Header';
 import s from './ProductDetails.module.scss';
 
 const ProductDetails = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const product = products.find(p => p.id === Number(id));
 
   if (!product) {
@@ -11,12 +13,29 @@ const ProductDetails = () => {
   }
 
   return (
-    <div className={s.productDetails}>
-      <h2>{product.name}</h2>
-      <p>Виробник:{product.manufacturer}</p>
-      <p>Категорія:{Array.isArray(product.category) ? product.category.join(', ') : product.category}</p>
-      <p>Опис:{product.description}</p>
-    </div>
+    <>
+      <Header />
+      <section className={s.sectionProductDetails}>
+        <div className={`container ${s.sectionProductDetails__Container}`}>
+          <div className={s.buttonBackContainer}>
+            <button onClick={() => navigate(-1)} className={s.buttonBack}>
+              Товар
+            </button>
+            <div className={s.decorationBorder}></div>
+            <h2 className={s.sectionProductDetailsTitle}>{product.name}</h2>
+          </div>
+
+          <p>Виробник: {product.manufacturer}</p>
+          <p>
+            Категорія:{' '}
+            {Array.isArray(product.category)
+              ? product.category.join(', ')
+              : product.category}
+          </p>
+          <p>Опис: {product.description}</p>
+        </div>
+      </section>
+    </>
   );
 };
 
