fa7c412d16e11a50caf6e8da4a2dedd13999d2a7
Chyharov <chyharov@gmail.com>
2025-03-10 23:42:27 +0100

    ADDED: markup and styles for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,78 @@
+import linkedinLogo from 'images/footer/linkedinLogo.svg';
+import facebookLogo from 'images/footer/facebookLogo.svg';
+import instagramLogo from 'images/footer/instagramLogo.svg';
+import logoFooter from 'images/footer/logoFooter.svg';
 import s from './Footer.module.scss';
 
 const Footer = () => {
   return (
     <footer className={s.footer}>
       <div className={`container ${s.footerContainer}`}>
-        <h2>Footer</h2>
+        <img className={s.logoFooter} src={logoFooter} alt="logoFooter" />
+        <ul className={s.footerSocailMediaList}>
+          <li className={s.footerSocailMediaList__item}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/"
+            >
+              <img
+                className={s.footerSocailMediaList__itemLogo}
+                src={linkedinLogo}
+                alt="linkedinLogo"
+              />
+            </a>
+          </li>
+          <li className={s.footerSocailMediaList__item}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.facebook.com/"
+            >
+              <img
+                className={s.footerSocailMediaList__itemLogo}
+                src={facebookLogo}
+                alt="facebookLogo"
+              />
+            </a>
+          </li>
+          <li className={s.footerSocailMediaList__item}>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/"
+            >
+              <img
+                className={s.footerSocailMediaList__itemLogo}
+                src={instagramLogo}
+                alt="instagramLogo"
+              />
+            </a>
+          </li>
+        </ul>
+
+        <nav>
+          <ul className={s.footerNavBarList}>
+            <li className={s.footerNavBarList__item}>
+              <p className={s.footerNavBarList__itemDescription}>Про нас</p>
+            </li>
+            <li className={s.footerNavBarList__item}>
+              <p className={s.footerNavBarList__itemDescription}>Товари</p>
+            </li>
+            <li className={s.footerNavBarList__item}>
+              <p className={s.footerNavBarList__itemDescription}>Партнери</p>
+            </li>
+            <li className={s.footerNavBarList__item}>
+              <p className={s.footerNavBarList__itemDescription}>Контакти</p>
+            </li>
+            <li className={s.footerNavBarList__item}>
+              <p className={s.footerNavBarList__itemDescription}>Блог</p>
+            </li>
+          </ul>
+              </nav>
+              
+              <h3 className={s.footerCopyrightTitle}>Всі права захищені © ANKONA 2025</h3>
+              <p className={s.footerCopyrightDescription}>Використання матеріалів сайту можливе лише з письмового дозволу правовласника та із зазначенням активного посилання на джерело</p>
       </div>
     </footer>
   );
