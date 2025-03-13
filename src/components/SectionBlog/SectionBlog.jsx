435ac3ac7206a47662e189468d7d0de8502bc4b8
Chyharov <chyharov@gmail.com>
2025-03-13 23:51:07 +0100

    ADDED: markup and styles for markup card on SectionBlog

--- src/components/SectionBlog/SectionBlog.jsx
+++ src/components/SectionBlog/SectionBlog.jsx
@@ -1,10 +1,22 @@
+import pigImg from 'images/blog/pig.jpg'
 import s from './SectionBlog.module.scss';
 
 const SectionBlog = () => {
   return (
     <section className={s.sectionBlog}>
           <div className={`container ${s.blogContainer}`}>
-              <h2>БЛОГ</h2>
+        <h2 className={s.blogTitle}>БЛОГ</h2>
+        <div className={s.blogBorder}></div>
+
+        <ul className={s.blogList}>
+          <li className={s.blogList__item}>
+            <img className={s.pigImg} src={pigImg} alt="pigImg" />
+            <h3 className={s.blogList__itemTitle}>Сіль глюконової кислоти</h3>
+            <div className={s.blogList__itemTitleBorder}></div>
+            <p className={s.blogList__itemDescription}>Вплив додавання сульфату глюконової кислоти на виробничі показники поросят та свиней на відгодівлі</p>
+          <button className={s.blogBtn} type='button'>Читати</button>
+          </li>
+        </ul>
         </div>
     </section>
   );
