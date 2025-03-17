e0a43dde9d00360ec81304da4d4a5065403e2f68
Chyharov <chyharov@gmail.com>
2025-03-17 23:47:21 +0100

    UPDATE: styles on Header for Desktop ver

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,26 @@
+import { useState, useEffect } from 'react';
 import MobileNavigation from './MobileNavigation';
 import Navigation from './Navigation';
 import s from './NavBar.module.scss';
 
-const NavMenu = ({ style }) => {
+const NavMenu = ({ style = {} }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1439);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <>
-      <div className={s.navBar} style={style} onClick={e => e.stopPropagation()}>
-        <nav className={s.nav}>
-          <MobileNavigation />
-          <Navigation />
-        </nav>
-      </div>
-    </>
+    <div className={s.navBar} style={style} onClick={e => e.stopPropagation()}>
+      <nav className={s.nav}>
+        {isMobile ? <MobileNavigation /> : <Navigation />}
+      </nav>
+    </div>
   );
 };
 
