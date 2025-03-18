f2b97df14c61ad399f9a0bd08ec8fa00491ea145
Chyharov <chyharov@gmail.com>
2025-03-18 23:43:18 +0100

    ADDED: logoDesk for desktop version

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,30 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import logo from 'images/header/logo.svg';
+import logoDesk from 'images/header/logoDesk.svg';
 import NavSearch from 'components/NavSearch/NavSearch';
 import NavBar from 'components/NavBar/NavBar';
 import LanguageSelect from 'components/LanguageSelect/LanguageSelect';
 import s from './Header.module.scss';
 
 const Header = ({ navSearchStyle, languageSelectStyle, navBarStyle, style }) => {
+  const [currentLogo, setCurrentLogo] = useState(window.innerWidth >= 1440 ? logoDesk : logo);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCurrentLogo(window.innerWidth >= 1440 ? logoDesk : logo);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <header className={s.header}>
       <div className={`container ${s.header__container}`}>
-        <Link to='/'><img className={s.logo} src={logo} alt="logo" /></Link>
+        <Link to='/'>
+          <img className={s.logo} src={currentLogo} alt="logo" />
+        </Link>
 
         <nav className={s.navigation__container} style={style}>
           <NavSearch style={navSearchStyle} />
