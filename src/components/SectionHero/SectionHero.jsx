45399d9d3a2ee5573a4a2247d74998deca09b060
Chyharov <chyharov@gmail.com>
2025-03-22 03:15:43 +0100

    ADDED: styles and markup on SectionHero for desktop version

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,24 +1,43 @@
+import { useEffect, useState } from 'react';
 import horseHeroImage from 'images/hero/horseHeroImage.jpg';
+import horseHeroImageDesktop from 'images/hero/horseHeroImageDesktop.jpg';
 import ankonaText from 'images/hero/ankonaText.svg';
+import ankonaTextDesktop from 'images/hero/ankonaTextDesktop.svg';
 import ButtonCallBack from 'components/ButtonCallBack/ButtonCallBack';
 import s from './SectionHero.module.scss';
 
 const SectionHero = () => {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 1440);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1440);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <section className={s.sectionHero}>
       <div className={s.heroPhotoContainer}>
+        <div className={s.horseHeroImageContainer}>
         <img
           className={s.horseHeroImage}
-          src={horseHeroImage}
+          src={isDesktop ? horseHeroImageDesktop : horseHeroImage}
           alt="horseHeroImage"
-        />
-        <img className={s.ankonaText} src={ankonaText} alt="ankonaText" />
+          />
+          </div>
+        <img className={s.ankonaText} src={isDesktop ? ankonaTextDesktop : ankonaText} alt="ankonaText" />
       </div>
       <div className={`container ${s.sectionHero__container}`}>
         <div className={s.heroBorder}>
           <div className={s.heroBorderDot}></div>
         </div>
         <p className={s.heroDescription}>Нова стратегія раціональної годівлі</p>
+        <div className={s.heroBorderDesk}>
+          <div className={s.heroBorderDotDesk}></div>
+        </div>
         <ButtonCallBack />
       </div>
     </section>
