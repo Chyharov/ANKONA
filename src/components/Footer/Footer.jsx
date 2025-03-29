e0c50a46b0c891c3dcc8eb5fa1f57c04b83839ab
Chyharov <chyharov@gmail.com>
2025-03-30 00:56:11 +0100

    ADDED: new markup and styles on Footer for desktop version

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
+import { Link as RouterLink } from 'react-router-dom';
 import linkedinLogo from 'images/footer/linkedinLogo.svg';
 import facebookLogo from 'images/footer/facebookLogo.svg';
 import instagramLogo from 'images/footer/instagramLogo.svg';
 import logoFooter from 'images/footer/logoFooter.svg';
 import s from './Footer.module.scss';
 
-const Footer = () => {
+const Footer = props => {
+  const animateFrom = { opacity: 0, y: -40 };
+  const animateTo = { opacity: 1, y: 0 };
+
   return (
     <footer className={s.footer}>
       <div className={`container ${s.footerContainer}`}>
-        <Link to='/'><img className={s.logoFooter} src={logoFooter} alt="logoFooter" /></Link>
+        <RouterLink to="/">
+          <img className={s.logoFooter} src={logoFooter} alt="logoFooter" />
+        </RouterLink>
         <ul className={s.footerSocailMediaList}>
           <li className={s.footerSocailMediaList__item}>
             <a
@@ -54,26 +60,97 @@ const Footer = () => {
 
         <nav>
           <ul className={s.footerNavBarList}>
+            <Link
+              onClick={() => props.isMobile && props.closeMobileMenu()}
+              className={s.footerNavBarList__item}
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.05 }}
+                className={s.footerNavBarList__itemDescription}
+              >
+                Про нас
+              </li>
+            </Link>
+            <Link
+              onClick={() => props.isMobile && props.closeMobileMenu()}
+              className={s.footerNavBarList__item}
+              to="goods"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.05 }}
+                className={s.footerNavBarList__itemDescription}
+              >
+                Товари
+              </li>
+            </Link>
+            <Link
+              onClick={() => props.isMobile && props.closeMobileMenu()}
+              className={s.footerNavBarList__item}
+              to="tradeMarks"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.05 }}
+                className={s.footerNavBarList__itemDescription}
+              >
+                Торгові марки
+              </li>
+            </Link>
+            <Link
+              onClick={() => props.isMobile && props.closeMobileMenu()}
+              className={s.footerNavBarList__item}
+              to="team"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              <li
+                initial={animateFrom}
+                animate={animateTo}
+                transition={{ delay: 0.05 }}
+                className={s.footerNavBarList__itemDescription}
+              >
+                Команда
+              </li>
+            </Link>
             <li className={s.footerNavBarList__item}>
-              <p className={s.footerNavBarList__itemDescription}>Про нас</p>
-            </li>
-            <li className={s.footerNavBarList__item}>
-              <p className={s.footerNavBarList__itemDescription}>Товари</p>
-            </li>
-            <li className={s.footerNavBarList__item}>
-              <p className={s.footerNavBarList__itemDescription}>Партнери</p>
-            </li>
-            <li className={s.footerNavBarList__item}>
-              <p className={s.footerNavBarList__itemDescription}>Контакти</p>
-            </li>
-            <li className={s.footerNavBarList__item}>
-              <p className={s.footerNavBarList__itemDescription}>Блог</p>
+              <RouterLink
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+                className={s.footerNavBarList__itemDescription}
+                to="/blog"
+              >
+                Блог
+              </RouterLink>
             </li>
           </ul>
-              </nav>
-              
-              <h3 className={s.footerCopyrightTitle}>Всі права захищені © ANKONA 2025</h3>
-              <p className={s.footerCopyrightDescription}>Використання матеріалів сайту можливе лише з письмового дозволу правовласника та із зазначенням активного посилання на джерело</p>
+        </nav>
+
+        <h3 className={s.footerCopyrightTitle}>
+          Всі права захищені © ANKONA 2025
+        </h3>
+        <p className={s.footerCopyrightDescription}>
+          Використання матеріалів сайту можливе лише з письмового дозволу
+          правовласника та із зазначенням активного посилання на джерело
+        </p>
       </div>
     </footer>
   );
