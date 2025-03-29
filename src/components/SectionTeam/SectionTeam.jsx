5cfec570c6e28c28e7dcf5021ecf8d4376ca7eae
Chyharov <chyharov@gmail.com>
2025-03-29 21:24:21 +0100

    UPDATE: array teamMembers

--- src/components/SectionTeam/SectionTeam.jsx
+++ src/components/SectionTeam/SectionTeam.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { teamMembers } from 'services/team'
 import s from './SectionTeam.module.scss';
 
 const SectionTeam = () => {
   const [selectedMember, setSelectedMember] = useState(null);
   const [contactInfo, setContactInfo] = useState({});
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth > 1440);
 
-  const handleSelectMember = id => {
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > 1439);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const handleSelectMember = (id) => {
     setSelectedMember(id === selectedMember ? null : id);
     setContactInfo({});
   };
 
   const handleShowContact = (id, type, value) => {
-    setContactInfo(prev => ({
+    setContactInfo((prev) => ({
       ...prev,
       [id]: {
         ...prev[id],
@@ -27,41 +37,27 @@ const SectionTeam = () => {
         <h2 className={s.teamTitle}>Наша команда</h2>
         <div className={s.teamBorder}></div>
         <ul className={s.teamList}>
-          {teamMembers.map(member => (
+          {teamMembers.map((member) => (
             <li
               key={member.id}
-              className={`${s.teamList__item} ${
-                selectedMember === member.id ? s.active : ''
-              }`}
+              className={`${s.teamList__item} ${selectedMember === member.id ? s.active : ''}`}
               onClick={() => handleSelectMember(member.id)}
             >
               <img
-                className={`${s.teamList__itemPhoto} ${
-                  selectedMember === member.id ? s.activ : ''
-                }`}
-                src={
-                  selectedMember === member.id
-                    ? member.activePhoto
-                    : member.photo
-                }
+                className={`${s.teamList__itemPhoto} ${selectedMember === member.id ? s.activ : ''}`}
+                src={isWideScreen || selectedMember !== member.id ? member.activePhoto : member.photo}
                 alt={member.name}
               />
               <div
-                className={`${s.teamNameContainer} ${
-                  selectedMember === member.id ? s.activ : ''
-                }`}
+                className={`${s.teamNameContainer} ${selectedMember === member.id ? s.activ : ''}`}
               >
                 <h3
-                  className={`${s.teamList__itemTitle} ${
-                    selectedMember === member.id ? s.active : ''
-                  }`}
+                  className={`${s.teamList__itemTitle} ${selectedMember === member.id ? s.active : ''}`}
                 >
                   {member.name}
                 </h3>
                 <p
-                  className={`${s.teamList__itemDescription} ${
-                    selectedMember === member.id ? s.active : ''
-                  }`}
+                  className={`${s.teamList__itemDescription} ${selectedMember === member.id ? s.active : ''}`}
                 >
                   {member.role}
                 </p>
@@ -72,14 +68,14 @@ const SectionTeam = () => {
                       <a
                         href={`mailto:${member.email}`}
                         className={s.btnTeamPhoneNumber}
-                        onClick={e => e.stopPropagation()}
+                        onClick={(e) => e.stopPropagation()}
                       >
                         {contactInfo[member.id].email}
                       </a>
                     ) : (
                       <button
                         className={s.btnTeamPhoneNumber}
-                        onClick={e => {
+                        onClick={(e) => {
                           e.stopPropagation();
                           handleShowContact(member.id, 'email', member.email);
                         }}
@@ -92,14 +88,14 @@ const SectionTeam = () => {
                       <a
                         href={`tel:${member.phone.replace(/\s|\(|\)|-/g, '')}`}
                         className={s.btnTeamEmail}
-                        onClick={e => e.stopPropagation()}
+                        onClick={(e) => e.stopPropagation()}
                       >
                         {contactInfo[member.id].phone}
                       </a>
                     ) : (
                       <button
                         className={s.btnTeamEmail}
-                        onClick={e => {
+                        onClick={(e) => {
                           e.stopPropagation();
                           handleShowContact(member.id, 'phone', member.phone);
                         }}
--- src/services/team.js
+++ src/services/team.js
@@ -1,15 +1,21 @@
 import polykovAndriy from 'images/team/polykovAndriy.jpg';
 import polykovAndriyIsActive from 'images/team/polykovAndriyIsActive.jpg';
+import polykovAndriyDesktop from 'images/team/polykovAndriyDesktop.jpg';
 import hodirevSerhiy from 'images/team/hodirevSerhiy.jpg';
 import hodirevSerhiyIsActive from 'images/team/hodirevSerhiyIsActive.jpg';
+import hodirevSerhiyDesktop from 'images/team/hodirevSerhiyDesktop.jpg';
 import hodirevaNatalia from 'images/team/hodirevaNatalia.jpg';
 import hodirevaNataliaIsActive from 'images/team/hodirevaNataliaIsActive.jpg';
+import hodirevaNataliaDesktop from 'images/team/hodirevaNataliaDesktop.jpg';
 import soloshenkoAndrii from 'images/team/soloshenkoAndrii.jpg';
 import soloshenkoAndriiIsActive from 'images/team/soloshenkoAndriiIsActive.jpg';
+import soloshenkoAndriiDesktop from 'images/team/soloshenkoAndriiDesktop.jpg';
 import komarVolodymyr from 'images/team/komarVolodymyr.jpg';
 import komarVolodymyrIsActive from 'images/team/komarVolodymyrIsActive.jpg';
+import komarVolodymyrDesktop from 'images/team/komarVolodymyrDesktop.jpg';
 import yanenkoYana from 'images/team/yanenkoYana.jpg';
 import yanenkoYanaIsActive from 'images/team/yanenkoYanaIsActive.jpg';
+import yanenkoYanaDesktop from 'images/team/yanenkoYanaDesktop.jpg';
 
 export const teamMembers = [
   {
@@ -18,6 +24,7 @@ export const teamMembers = [
     role: 'Директор',
     photo: polykovAndriy,
     activePhoto: polykovAndriyIsActive,
+    photoDesktop: polykovAndriyDesktop,
     email: 'info@ankona.com.ua',
     phone: '+38 (098) 973 03 91',
   },
@@ -27,6 +34,7 @@ export const teamMembers = [
     role: 'Керівник/технолог напрямку ВРХ',
     photo: hodirevSerhiy,
     activePhoto: hodirevSerhiyIsActive,
+    photoDesktop: hodirevSerhiyDesktop,
     email: 'svhodyrev@ankona.com.ua',
     phone: '+38 (050) 481 53 20',
   },
@@ -36,6 +44,7 @@ export const teamMembers = [
     role: 'Керівник відділу з продажів напрямку ВРХ',
     photo: hodirevaNatalia,
     activePhoto: hodirevaNataliaIsActive,
+    photoDesktop: hodirevaNataliaDesktop,
     email: 'nvhodyreva@ankona.com.ua',
     phone: '+38 (050) 777 09 75',
   },
@@ -45,6 +54,7 @@ export const teamMembers = [
     role: 'Консультант з годівлі свиней',
     photo: soloshenkoAndrii,
     activePhoto: soloshenkoAndriiIsActive,
+    photoDesktop: soloshenkoAndriiDesktop,
     email: 'andriy.soloshenko@ankona.com.ua',
     phone: '+38 (098) 604 53 38',
   },
@@ -54,6 +64,7 @@ export const teamMembers = [
     role: 'Консультант з годівлі свиней',
     photo: komarVolodymyr,
     activePhoto: komarVolodymyrIsActive,
+    photoDesktop: komarVolodymyrDesktop,
     email: 'volodymyr.komar@ankona.com.ua',
     phone: '+38 (095) 408 95 25',
   },
@@ -63,6 +74,7 @@ export const teamMembers = [
     role: 'Керівник роздрібного відділу',
     photo: yanenkoYana,
     activePhoto: yanenkoYanaIsActive,
+    photoDesktop: yanenkoYanaDesktop,
     email: 'retail@ankona.com.ua',
     phone: '+38 (077) 115 11 55',
   },
