0a227de56c75ba455efc3e51724582f7f3edbbb9
Chyharov <chyharov@gmail.com>
2025-03-29 18:12:19 +0100

    UPDATE: logic for pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,16 +1,57 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import arrowLeft from 'images/goods/arrowLeft.svg';
 import arrowRight from 'images/goods/arrowRight.svg';
 import s from './Pagination.module.scss';
 
 const Pagination = ({ currentPage, totalPages, onPageChange, onLoadMore, hasMoreItems }) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1439);
+
   useEffect(() => {
-    const handleResize = () => window.innerWidth > 1439;
+    const handleResize = () => setIsMobile(window.innerWidth <= 1439);
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  const renderPaginationButtons = () => {
+  const renderPaginationButtonsMobile = () => {
+    const pages = [];
+    const firstPage = 1;
+    const lastPage = totalPages;
+    const middlePage = Math.ceil(totalPages / 2);
+
+    pages.push(firstPage);
+
+    if (currentPage > 2 && currentPage < middlePage) {
+      pages.push(currentPage);
+    } else {
+      pages.push(2);
+    }
+
+    pages.push("...");
+
+    if (currentPage !== lastPage) {
+      pages.push(currentPage >= middlePage ? currentPage : middlePage);
+    } else {
+      pages.push(middlePage);
+    }
+
+    pages.push("...");
+    pages.push(lastPage);
+
+    return pages.map((page, index) => (
+      <button
+        key={index}
+        className={
+          page === "..." ? s.ellipsis : page === currentPage ? s.numerPaginationBtn : s.numerPaginationBtnInactive
+        }
+        onClick={() => typeof page === "number" && onPageChange(page)}
+        disabled={page === "..."}
+      >
+        {page}
+      </button>
+    ));
+  };
+
+  const renderPaginationButtonsDesktop = () => {
     const pages = [];
     const firstPage = 1;
     const lastPage = totalPages;
@@ -38,11 +79,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange, onLoadMore, hasMore
       <button
         key={index}
         className={
-          page === '...'
-            ? s.ellipsis
-            : page === currentPage
-            ? s.numerPaginationBtn
-            : s.numerPaginationBtnInactive
+          page === '...' ? s.ellipsis : page === currentPage ? s.numerPaginationBtn : s.numerPaginationBtnInactive
         }
         onClick={() => typeof page === 'number' && onPageChange(page)}
         disabled={page === '...'}
@@ -68,7 +105,7 @@ const Pagination = ({ currentPage, totalPages, onPageChange, onLoadMore, hasMore
           <img src={arrowLeft} alt="arrowLeft" />
         </button>
 
-        {renderPaginationButtons()}
+        {isMobile ? renderPaginationButtonsMobile() : renderPaginationButtonsDesktop()}
 
         <button
           className={s.buttonPaginationRight}
