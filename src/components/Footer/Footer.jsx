0ee002b1ecb0f45783fbdb1dc2d1f7e1a2f5c1e7
Chyharov <chyharov@gmail.com>
2025-03-30 01:47:26 +0100

    ADDED: new markup and styles on Footer for desktop version v2

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,144 +13,148 @@ const Footer = props => {
   return (
     <footer className={s.footer}>
       <div className={`container ${s.footerContainer}`}>
-        <RouterLink to="/">
-          <img className={s.logoFooter} src={logoFooter} alt="logoFooter" />
-        </RouterLink>
-        <ul className={s.footerSocailMediaList}>
-          <li className={s.footerSocailMediaList__item}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/"
-            >
-              <img
-                className={s.footerSocailMediaList__itemLogo}
-                src={linkedinLogo}
-                alt="linkedinLogo"
-              />
-            </a>
-          </li>
-          <li className={s.footerSocailMediaList__item}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.facebook.com/"
-            >
-              <img
-                className={s.footerSocailMediaList__itemLogo}
-                src={facebookLogo}
-                alt="facebookLogo"
-              />
-            </a>
-          </li>
-          <li className={s.footerSocailMediaList__item}>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.instagram.com/"
-            >
-              <img
-                className={s.footerSocailMediaList__itemLogo}
-                src={instagramLogo}
-                alt="instagramLogo"
-              />
-            </a>
-          </li>
-        </ul>
+        <div className={s.footerContainerDesktop}>
+          <RouterLink to="/" className={s.linkLogo}>
+            <img className={s.logoFooter} src={logoFooter} alt="logoFooter" />
+          </RouterLink>
 
-        <nav>
-          <ul className={s.footerNavBarList}>
-            <Link
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-              className={s.footerNavBarList__item}
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              <li
-                initial={animateFrom}
-                animate={animateTo}
-                transition={{ delay: 0.05 }}
-                className={s.footerNavBarList__itemDescription}
+          <ul className={s.footerSocailMediaList}>
+            <li className={s.footerSocailMediaList__item}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/"
               >
-                Про нас
-              </li>
-            </Link>
-            <Link
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-              className={s.footerNavBarList__item}
-              to="goods"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              <li
-                initial={animateFrom}
-                animate={animateTo}
-                transition={{ delay: 0.05 }}
-                className={s.footerNavBarList__itemDescription}
+                <img
+                  className={s.footerSocailMediaList__itemLogo}
+                  src={linkedinLogo}
+                  alt="linkedinLogo"
+                />
+              </a>
+            </li>
+            <li className={s.footerSocailMediaList__item}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.facebook.com/"
               >
-                Товари
-              </li>
-            </Link>
-            <Link
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-              className={s.footerNavBarList__item}
-              to="tradeMarks"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              <li
-                initial={animateFrom}
-                animate={animateTo}
-                transition={{ delay: 0.05 }}
-                className={s.footerNavBarList__itemDescription}
+                <img
+                  className={s.footerSocailMediaList__itemLogo}
+                  src={facebookLogo}
+                  alt="facebookLogo"
+                />
+              </a>
+            </li>
+            <li className={s.footerSocailMediaList__item}>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://www.instagram.com/"
               >
-                Торгові марки
-              </li>
-            </Link>
-            <Link
-              onClick={() => props.isMobile && props.closeMobileMenu()}
-              className={s.footerNavBarList__item}
-              to="team"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
-            >
-              <li
-                initial={animateFrom}
-                animate={animateTo}
-                transition={{ delay: 0.05 }}
-                className={s.footerNavBarList__itemDescription}
-              >
-                Команда
-              </li>
-            </Link>
-            <li className={s.footerNavBarList__item}>
-              <RouterLink
-                onClick={() => props.isMobile && props.closeMobileMenu()}
-                className={s.footerNavBarList__itemDescription}
-                to="/blog"
-              >
-                Блог
-              </RouterLink>
+                <img
+                  className={s.footerSocailMediaList__itemLogo}
+                  src={instagramLogo}
+                  alt="instagramLogo"
+                />
+              </a>
             </li>
           </ul>
-        </nav>
 
-        <h3 className={s.footerCopyrightTitle}>
-          Всі права захищені © ANKONA 2025
-        </h3>
-        <p className={s.footerCopyrightDescription}>
-          Використання матеріалів сайту можливе лише з письмового дозволу
-          правовласника та із зазначенням активного посилання на джерело
-        </p>
+          <nav className={s.footerNavBarList}>
+            <ul className={s.footerBarList}>
+              <Link
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+                className={s.footerBarList__item}
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.05 }}
+                  className={s.footerBarList__itemDescription}
+                >
+                  Про нас
+                </li>
+              </Link>
+              <Link
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+                className={s.footerBarList__item}
+                to="goods"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.05 }}
+                  className={s.footerBarList__itemDescription}
+                >
+                  Товари
+                </li>
+              </Link>
+              <Link
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+                className={s.footerBarList__item}
+                to="tradeMarks"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.05 }}
+                  className={s.footerBarList__itemDescription}
+                >
+                  Торгові марки
+                </li>
+              </Link>
+              <Link
+                onClick={() => props.isMobile && props.closeMobileMenu()}
+                className={s.footerBarList__item}
+                to="team"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                <li
+                  initial={animateFrom}
+                  animate={animateTo}
+                  transition={{ delay: 0.05 }}
+                  className={s.footerBarList__itemDescription}
+                >
+                  Команда
+                </li>
+              </Link>
+              <li className={s.footerBarList__item}>
+                <RouterLink
+                  onClick={() => props.isMobile && props.closeMobileMenu()}
+                  className={s.footerBarList__itemDescription}
+                  to="/blog"
+                >
+                  Блог
+                </RouterLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className={s.privacyContainer}>
+          <h3 className={s.footerCopyrightTitle}>
+            Всі права захищені © ANKONA 2025
+          </h3>
+          <p className={s.footerCopyrightDescription}>
+            Використання матеріалів сайту можливе лише з письмового дозволу
+            правовласника та із зазначенням активного посилання на джерело
+          </p>
+        </div>
       </div>
     </footer>
   );
