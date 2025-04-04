2c744425e3952c58ae2fec23619ac584ca26c29d
Chyharov <chyharov@gmail.com>
2025-04-04 15:23:57 +0200

    FIX: routes on project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,16 @@
-import React, { lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-const MainPage = lazy(() => import('pages/MainPage/MainPage'));
-const ProductDetails = lazy(() => import('components/ProductDetails/ProductDetails'));
-const BlogPage = lazy(() => import('pages/BlogPage/BlogPage'));
-const BlogPostPage = lazy(() => import('pages/BlogPostPage/BlogPostPage'));
-const PrivacyPage = lazy(() => import('pages/PrivacyPage/PrivacyPage'));
+import React, { lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
+import { SearchProvider } from "components/SearchContext"; // Додай провайдер
 
+const MainPage = lazy(() => import("pages/MainPage/MainPage"));
+const ProductDetails = lazy(() => import("components/ProductDetails/ProductDetails"));
+const BlogPage = lazy(() => import("pages/BlogPage/BlogPage"));
+const BlogPostPage = lazy(() => import("pages/BlogPostPage/BlogPostPage"));
+const PrivacyPage = lazy(() => import("pages/PrivacyPage/PrivacyPage"));
 
 export const App = () => {
   return (
-    <>
+    <SearchProvider> {/* Тепер у всіх дочірніх компонентах буде доступ до SearchContext */}
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<MainPage />} />
@@ -19,6 +20,6 @@ export const App = () => {
           <Route path="/privacy" element={<PrivacyPage />} />
         </Routes>
       </Suspense>
-    </>
+    </SearchProvider>
   );
 };
