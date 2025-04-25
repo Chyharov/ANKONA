8830e48ab8127bc53e0dcb12e373a9ceae17a73c
Chyharov <chyharov@gmail.com>
2025-04-25 21:15:08 +0200

    ADDED: navItems for PrivacyPage

--- src/pages/PrivacyPage/PrivacyPage.jsx
+++ src/pages/PrivacyPage/PrivacyPage.jsx
@@ -2,6 +2,7 @@ import { useState, useCallback } from 'react';
 import Header from 'components/Header/Header';
 import SectionPrivacy from 'components/SectionPrivacy/SectionPrivacy';
 import Footer from 'components/Footer/Footer';
+import translations from 'components/LanguageSelect/translations';
 
 const PrivacyPage = () => {
   const [language, setLanguage] = useState('ua');
@@ -14,10 +15,20 @@ const PrivacyPage = () => {
         }
       },
       [language]
-    );
+  );
+  
+  const t = translations.navlink[language];
+  const navItems = [
+    {
+      type: 'route',
+      to: '/',
+      label: t.main,
+      delay: 0.05,
+    },
+  ];
   return (
     <>
-      <Header language={language} onLanguageChange={handleLanguageChange} navSearchStyle={{ display: 'none' }} languageSelectStyle={{ display: 'none' }}/>
+      <Header language={language} onLanguageChange={handleLanguageChange} navSearchStyle={{ display: 'none' }} languageSelectStyle={{ display: 'none' }} navItems={navItems} />
       <main>
         <SectionPrivacy language={language}/>
       </main>
